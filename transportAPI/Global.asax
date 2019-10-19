c11d29ea07eeb0fb7b4b5748b09d0d88db50e6c6
wlee075 <wymanleejun@gmail.com>
2019-10-19 12:03:56 +0800

    init file

---
+++ transportAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="transportAPI.Global" Language="C#" %>
---
+++ transportAPI/Site.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Net;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace transportAPI
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strurltest = String.Format("https://jsonplaceholder.typicode.com/posts/1");
+            WebRequest requestObjGet = WebRequest.Create(strurltest);
+            requestObjGet.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse)requestObjGet.GetResponse();
+            string strresulttest = null;
+            using (Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                strresulttest = sr.ReadToEnd();
+                //reminder: remove after prod. GET is working.
+                System.Diagnostics.Debug.WriteLine(strresulttest);
+                sr.Close();
+            }
+        }
+    }
+}
